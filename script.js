900d67391b826f9e3d9992475e46a4e1c3aee48a
gustavkis <gustavkis.bp@gmail.com>
2022-12-10 23:34:50 +0100

    azonosit() függvény kidolgozása

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function uj() {
     location.reload();
   }
+
+
+
+  function azonosit() {
+    arSapkasAr = parseInt(document.getElementById("arSapkasAr").value);
+    piaciAr = parseInt(document.getElementById("piaciAr").value);
+    tankoltMennyiseg = parseInt(
+      document.getElementById("tankoltMennyiseg").value
+    );
+  
+    var piaciAron = 20 * arSapkasAr + (tankoltMennyiseg - 20) * piaciAr;
+    var arSapkasAron = arSapkasAr * tankoltMennyiseg;
+    var arSapkasAron1 = arSapkasAr * 20;
+    var fizetendoPiaciAr = (tankoltMennyiseg - 20) * piaciAr;
+    var fizetendoArSapkasAr = arSapkasAron;
+    var nincsPiaciAr = 0;
+  
+    if (tankoltMennyiseg <= 20) {
+      return [arSapkasAron, nincsPiaciAr, fizetendoArSapkasAr];
+    } else {
+      return [arSapkasAron1, piaciAron, fizetendoPiaciAr];
+    }
+  }
