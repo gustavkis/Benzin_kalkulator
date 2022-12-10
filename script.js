23ed01383099f821393a0e76dccabe821e8d6105
gustavkis <gustavkis.bp@gmail.com>
2022-12-10 23:36:54 +0100

    megtalal() függvény kidolgozása

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ function uj() {
     location.reload();
   }
 
+  function megtalal() {
+    var arak = azonosit();
+    var arSapkasAron = arak[0];
+    kiiras(arSapkasAron);
+    var nincsPiaciAr = arak[1];
+    kiiras1(nincsPiaciAr);
+    var fizetendoArSapkasAr = arak[2];
+    kiiras2(fizetendoArSapkasAr);
+    var arSapkasAron1 = arak[0];
+    kiiras3(arSapkasAron1);
+    var piaciAron = arak[1];
+    kiiras4(piaciAron);
+    var fizetendoPiaciAr = arak[2];
+    kiiras5(fizetendoPiaciAr);
+  }
 
 
   function azonosit() {
